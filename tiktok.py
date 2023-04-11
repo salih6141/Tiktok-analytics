b2a3d9b44b7b39305a2d60a301e3c708c38692c8
komutS <komutsalih61@icloud.com>
2023-04-11 07:49:44 +0200

    installed libraries and did preperation for api call

--- tiktok.py
+++ tiktok.py
@@ -1,1 +1,9 @@
 from TikTokApi import TikTokApi as tiktok #tiktokapi unofficial from github
+import json
+
+#api setup
+verifyFp = "verify_lgbubcfd_dk7KGyI7_g0kv_4gJX_Bh2J_WLZIEvS6y6Cb"
+api = tiktok.get_instance(costum_verifyFp=verifyFp, use_test_endpoints=True)
+trending = api.by_hashtag('Belgium')
+
+
